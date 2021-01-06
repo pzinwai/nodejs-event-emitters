86d95ba21aae7aa9706054ef7703fb7a553b066c
Phyo Zin Wai <phyozinwai@gmail.com>
2021-01-06 09:50:52 +0800

    Init commit.

---
+++ firstListener.js
@@ -0,0 +1,15 @@
+const TicketManager = require("./ticketManager");
+
+const ticketManager = new TicketManager(10);
+
+ticketManager.on("buy", () => {
+    console.log("Someone bought a ticket!");
+});
+
+ticketManager.once("buy", () => {
+  console.log("This is only called once");
+});
+
+ticketManager.buy("test@email.com", 20);
+ticketManager.buy("test@email.com", 20);
+
